48d1e6f3a3ea4b8729f0a5a8fce897170ad9e5d9
Shivam Kanathe <shivam15alphawizz@gmail.com>
2023-03-06 11:33:14 +0530

    one image change to live

--- lib/Utils/api_path.dart
+++ lib/Utils/api_path.dart
@@ -1,9 +1,9 @@
 
 class ApiPath{
 
-  static const String baseUrl ="https://developmentalphawizz.com/e_gate/Api/";
-  static const String imgUrl = 'https://developmentalphawizz.com/e_gate/uploads/';
-  static const String imgUrl1 = 'https://developmentalphawizz.com/e_gate/uploads/profile_pics/';
+  static const String baseUrl ="https://egateschool.co.in/Api/";
+  static const String imgUrl = 'https://egateschool.co.in/uploads/';
+  static const String imgUrl1 = 'https://egateschool.co.in/profile_pics/';
 
   static const String login = baseUrl +'login';
   static const String update_profile = baseUrl+'update_profile';
